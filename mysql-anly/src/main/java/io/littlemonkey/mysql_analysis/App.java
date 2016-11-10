ce9a6a1a045e737db6c39146a1ea339f4fc7edb1
Administrator <Administrator@WIN-NC8G918NTKF>
2016-11-10 17:56:21 +0800

    1

---
+++ mysql-anly/src/main/java/io/littlemonkey/mysql_analysis/App.java
@@ -0,0 +1,13 @@
+package io.littlemonkey.mysql_analysis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
